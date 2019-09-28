fbb88af404c142976a0e0b5dd4bb1ca7da7c8fff
Thomas Pongratz <pliss@gmx.de>
2019-09-28 19:30:13 +0200

    versuch3b bearbeitet

---
+++ versuch3b/src/heron.h
@@ -0,0 +1,14 @@
+/*
+ * heron.h
+ *
+ *  Created on: 28.09.2019
+ *      Author: pliss
+ */
+
+#ifndef HERON_H_
+#define HERON_H_
+
+#define EPSILON 1e-5
+double heron(unsigned);
+
+#endif /* HERON_H_ */
